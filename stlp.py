7a6ea0a30d065033907dc0834b9bbfa4180f8be3
DARK-NI <88588581+DARK-NI@users.noreply.github.com>
2021-12-28 15:31:53 +0600

    Add files via upload

---
+++ stlp.py
@@ -0,0 +1,553 @@
+import time
+import sys
+import os
+
+# Reset
+color_off="\033[0m"       # Text Reset
+
+#Regular colors
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+# Bold
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+
+# Background
+on_black="\033[40m"       # Black
+on_red="\033[41m"         # Red
+on_green="\033[42m"       # Green
+on_yellow="\033[43m"      # Yellow
+on_blue="\033[44m"        # Blue
+on_purple="\033[45m"      # Purple
+on_cyan="\033[46m"        # Cyan
+on_white="\033[47m"       # White
+
+
+os.system("clear")
+
+
+for i in range(100):
+  os.system("clear")
+  print("""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            \033[1;35m  LOADING.......""" , end=" ")
+
+  print(str(i+1) + "%\033[0m")
+  time.sleep(0.0)
+
+time.sleep(2)
+
+os.system("clear")
+w = """
+
+
+
+
+
+
+
+
+\033[1;31m                                           
+                                           
+    WWWWWWWW                           WWWWWWWW
+    W::::::W                           W::::::W
+    W::::::W                           W::::::W
+    W::::::W                           W::::::W
+     W:::::W           WWWWW           W:::::W 
+      W:::::W         W:::::W         W:::::W  
+       W:::::W       W:::::::W       W:::::W   
+        W:::::W     W:::::::::W     W:::::W    
+         W:::::W   W:::::W:::::W   W:::::W     
+          W:::::W W:::::W W:::::W W:::::W      
+           W:::::W:::::W   W:::::W:::::W       
+            W:::::::::W     W:::::::::W        
+             W:::::::W       W:::::::W         
+              W:::::W         W:::::W          
+               W:::W           W:::W           
+                WWW             WWW        \033[0m    
+                                           
+                                           
+                                           
+                                           
+                                           
+                                           """
+
+e = """
+
+
+
+
+
+
+
+                   
+                      
+            EEEEEEEEEEEEEEEEEEEEEE
+            E::::::::::::::::::::E
+            E::::::::::::::::::::E
+            EE::::::EEEEEEEEE::::E
+              E:::::E       EEEEEE
+              E:::::E             
+              E::::::EEEEEEEEEE   
+              E:::::::::::::::E   
+              E:::::::::::::::E   
+              E::::::EEEEEEEEEE   
+              E:::::E             
+              E:::::E       EEEEEE
+            EE::::::EEEEEEEE:::::E
+            E::::::::::::::::::::E
+            E::::::::::::::::::::E
+            EEEEEEEEEEEEEEEEEEEEEE
+                             
+                      
+                      """
+                      
+                      
+                      
+                      
+l = """
+
+
+
+
+
+
+
+                        
+                        
+              LLLLLLLLLLL             
+              L:::::::::L             
+              L:::::::::L             
+              LL:::::::LL             
+                L:::::L               
+                L:::::L               
+                L:::::L               
+                L:::::L               
+                L:::::L               
+                L:::::L               
+                L:::::L               
+                L:::::L         LLLLLL
+              LL:::::::LLLLLLLLL:::::L
+              L::::::::::::::::::::::L
+              L::::::::::::::::::::::L
+              LLLLLLLLLLLLLLLLLLLLLLLL
+                      
+                        """
+                        
+                        
+                        
+                        
+                        
+c = """
+
+
+
+
+
+
+
+
+                     
+                     
+                    CCCCCCCCCCCCC
+                 CCC::::::::::::C
+               CC:::::::::::::::C
+              C:::::CCCCCCCC::::C
+             C:::::C       CCCCCC
+            C:::::C              
+            C:::::C              
+            C:::::C              
+            C:::::C              
+            C:::::C              
+            C:::::C              
+             C:::::C       CCCCCC
+              C:::::CCCCCCCC::::C
+               CC:::::::::::::::C
+                 CCC::::::::::::C
+                    CCCCCCCCCCCCC
+                     
+                     """
+                     
+                     
+                     
+                     
+                     
+o = """
+
+
+
+
+
+
+
+                   
+                   
+                   OOOOOOOOO     
+                 OO:::::::::OO   
+               OO:::::::::::::OO 
+              O:::::::OOO:::::::O
+              O::::::O   O::::::O
+              O:::::O     O:::::O
+              O:::::O     O:::::O
+              O:::::O     O:::::O
+              O:::::O     O:::::O
+              O:::::O     O:::::O
+              O:::::O     O:::::O
+              O::::::O   O::::::O
+              O:::::::OOO:::::::O
+               OO:::::::::::::OO 
+                 OO:::::::::OO   
+                   OOOOOOOOO     
+                   """
+                   
+                   
+                   
+                   
+                   
+                   
+m = """
+
+
+
+
+
+
+
+                               
+                               
+          MMMMMMMM               MMMMMMMM
+          M:::::::M             M:::::::M
+          M::::::::M           M::::::::M
+          M:::::::::M         M:::::::::M
+          M::::::::::M       M::::::::::M
+          M:::::::::::M     M:::::::::::M
+          M:::::::M::::M   M::::M:::::::M
+          M::::::M M::::M M::::M M::::::M
+          M::::::M  M::::M::::M  M::::::M
+          M::::::M   M:::::::M   M::::::M
+          M::::::M    M:::::M    M::::::M
+          M::::::M     MMMMM     M::::::M
+          M::::::M               M::::::M
+          M::::::M               M::::::M
+          M::::::M               M::::::M
+          MMMMMMMM               MMMMMMMM
+                               
+                               
+                               """
+
+
+to = """
+
+
+
+
+
+
+
+                                          
+                                          
+    TTTTTTTTTTTTTTTTTTTTTTT     OOOOOOOOO     
+    T:::::::::::::::::::::T   OO:::::::::OO   
+    T:::::::::::::::::::::T OO:::::::::::::OO 
+    T:::::TT:::::::TT:::::TO:::::::OOO:::::::O
+    TTTTTT  T:::::T  TTTTTTO::::::O   O::::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O:::::O     O:::::O
+            T:::::T        O::::::O   O::::::O
+          TT:::::::TT      O:::::::OOO:::::::O
+          T:::::::::T       OO:::::::::::::OO 
+          T:::::::::T         OO:::::::::OO   
+          TTTTTTTTTTT           OOOOOOOOO     
+                                          
+                                         """ 
+                                          
+                                          
+                                          
+                                          
+                                          
+
+stlp = """
+
+
+
+
+
+
+
+
+
+      :'######::'########:'##:::::::'########::
+      '##... ##:... ##..:: ##::::::: ##.... ##:
+       ##:::..::::: ##:::: ##::::::: ##:::: ##:
+      . ######::::: ##:::: ##::::::: ########::
+      :..... ##:::: ##:::: ##::::::: ##.....:::
+      '##::: ##:::: ##:::: ##::::::: ##::::::::
+      . ######::::: ##:::: ########: ##::::::::
+      :......::::::..:::::........::..:::::::::
+      
+      
+           Coder: Md Nazmul Islam (DARK-NI)
+                       ×××××××
+           Spamming & Termux Learning Point
+      
+      
+"""
+
+tnx = """
+\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
+\033[1;34m
+
+
+
+
+
+
+
+
+
+
+
+
+        ___ _  _ _  _    ____ ____ ____ 
+         |  |\ |  \/     |___ |  | |__/ 
+         |  | \| _/\_    |    |__| |  \ 
+                                
+
+
+      ____ ____ _  _ ___  _    ____ ___ ____ 
+      |    |  | |\/| |__] |    |___  |  |___ 
+      |___ |__| |  | |    |___ |___  |  |___ 
+                                           
+
+
+                                    
+                                    
+\033[1;31m          1111111        kkkkkkkk           
+ \033[1;32m        1::::::1        k::::::k           
+\033[1;31m        1:::::::1        k::::::k           
+\033[1;32m        111:::::1        k::::::k           
+\033[1;31m           1::::1         k:::::k    kkkkkkk
+\033[1;32m           1::::1         k:::::k   k:::::k 
+\033[1;31m           1::::1         k:::::k  k:::::k  
+\033[1;32m           1::::l         k:::::k k:::::k   
+\033[1;31m           1::::l         k::::::k:::::k    
+\033[1;32m           1::::l         k:::::::::::k     
+\033[1;31m           1::::l         k:::::::::::k     
+ \033[1;32m          1::::l         k::::::k:::::k    
+ \033[1;31m       111::::::111     k::::::k k:::::k   
+ \033[1;32m       1::::::::::1     k::::::k  k:::::k  
+\033[1;31m        1::::::::::1     k::::::k   k:::::k 
+\033[1;32m        111111111111     kkkkkkkk    kkkkkkk
+ \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ """                                
+                                    
+                                    
+                                    
+tnx1 = """
+
+
+
+        ___ _  _ _  _    ____ ____ ____ 
+         |  |\ |  \/     |___ |  | |__/ 
+         |  | \| _/\_    |    |__| |  \ 
+                                
+
+
+      ____ ____ _  _ ___  _    ____ ___ ____ 
+      |    |  | |\/| |__] |    |___  |  |___ 
+      |___ |__| |  | |    |___ |___  |  |___ 
+                                           
+"""
+
+k1 = """
+
+
+                                    
+\033[1;31m          1111111        kkkkkkkk           
+ \033[1;32m        1::::::1        k::::::k           
+\033[1;31m        1:::::::1        k::::::k           
+\033[1;32m        111:::::1        k::::::k           
+\033[1;31m           1::::1         k:::::k    kkkkkkk
+\033[1;32m           1::::1         k:::::k   k:::::k 
+\033[1;31m           1::::1         k:::::k  k:::::k  
+\033[1;32m           1::::l         k:::::k k:::::k   
+\033[1;31m           1::::l         k::::::k:::::k    
+\033[1;32m           1::::l         k:::::::::::k     
+\033[1;31m           1::::l         k:::::::::::k     
+ \033[1;32m          1::::l         k::::::k:::::k    
+ \033[1;31m       111::::::111     k::::::k k:::::k   
+ \033[1;32m       1::::::::::1     k::::::k  k:::::k  
+\033[1;31m        1::::::::::1     k::::::k   k:::::k 
+\033[1;32m        111111111111     kkkkkkkk    kkkkkkk
+"""
+
+print(w)
+time.sleep(1)
+os.system("clear")
+
+print(bgreen + e)
+time.sleep(1)
+os.system("clear")
+
+print(byellow + l)
+time.sleep(1)
+os.system("clear")
+
+print(bblue + l)
+time.sleep(1)
+os.system("clear")
+
+print(bpurple + c)
+time.sleep(1)
+os.system("clear")
+
+print(bcyan + o)
+time.sleep(1)
+os.system("clear")
+
+print(bred + m)
+time.sleep(1)
+os.system("clear")
+
+print(bgreen + e)
+time.sleep(1)
+os.system("clear")
+
+print(bpurple + to)
+time.sleep(1)
+os.system("clear")
+
+for i in range(5):
+  os.system("clear")
+  time.sleep(0.08)
+  print(bgreen + stlp)
+  time.sleep(0.08)
+  
+time.sleep(1.5)
+
+print("\n")
+st1 =  """*   *
+           *
+                *             *   '*
+  *
+                *
+                       *               *   '*
+             *
+                *                 *   '*
+       *
+                *
+                   *           *   '*
+           *
+                   *                                     *
+
+                              *
+  *   '*
+                        *
+                                  *
+                                *
+                         *
+                  *
+                                  *   '*
+           *
+      *             *   '*
+           *
+                                   *               *   '*
+           *
+                  *                 *   '*
+                 *
+                       *           *   '*
+           *
+                *                                     *
+
+                              *
+                              **   '*
+                                  *
+                                           *
+            *
+                    *
+        *           *
+                  *                 *   '*
+                 *
+                       *           *   '*
+           *
+                *                                     *
+
+                              *
+"""
+
+
+for i in range(2):
+  print(byellow + str(st1))
+  time.sleep(0.5)
+  print(bgreen + str(st1))
+  time.sleep(0.5)
+  print(bblue + str(st1))
+  time.sleep(0.5)
+  print(bcyan + str(st1))
+  time.sleep(0.5)
+  print(bred + str(st1))
+  time.sleep(0.5)
+  print(bpurple + str(st1))
+  time.sleep(0.5)
+  os.system("clear")
+  
+for c in tnx:
+  sys.stdout.write(c)
+  sys.stdout.flush()
+  time.sleep(0.006)
+os.system("clear")
+
+
+for i in range(5):
+  os.system("clear")
+  time.sleep(0.08)
+  print(bblue + tnx1)
+  print(k1)
+  time.sleep(0.08)
+  
+  
+
